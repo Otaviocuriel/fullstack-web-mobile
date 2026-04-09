5839092c4f0ec89e59c2c8c0bfc064965622771c
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-08 22:00:27 -0300

    Funcionando

--- Exercicio/CarrinhoCompra.js
+++ Exercicio/CarrinhoCompra.js
@@ -1,28 +1,28 @@
 class Carrinho {
   constructor() {
     this.produtos = [];
-  }
+  };
 
   adicionar(nome, preco) {
     this.produtos.push({ nome, preco });
-  }
+  };
 
   listar() {
     this.produtos.forEach(produto => {
       console.log(`Produto: ${produto.nome} | Preço: R$ ${produto.preco}`);
     });
-  }
+  };
 
   total() {
     return this.produtos.reduce((soma, produto) => soma + produto.preco, 0);
-  }
-}
+  };
+};
 
 
 const carrinho = new Carrinho();
-carrinho.adicionar("álbum da Copa do Mundo 2026", 80.00);
-carrinho.adicionar(" figurinhas da Copa do Mundo de 2026", 7.00);
-carrinho.adicionar("ingressos para a Copa do Mundo de 2026", 9000.00);
+carrinho.adicionar("Álbum da Copa do Mundo 2026", 80.00);
+carrinho.adicionar("Figurinhas da Copa do Mundo de 2026", 7.00);
+carrinho.adicionar("Ingressos para a Copa do Mundo de 2026", 9000.00);
 
 carrinho.listar();
 console.log(`Total: R$ ${carrinho.total().toFixed(2)}`);
