135d91ac811dcbd9101cdd12b944f4c1c0e517d9
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-18 16:41:41 -0300

    Exercicio Variantes em ts

---
+++ Exercicio/Variaveis.ts
@@ -0,0 +1,12 @@
+const nome: string = "Otavio";
+const idade: number = 18;
+const matriculado: boolean = true;
+const cidade: string = "Araras";
+
+
+console.log(nome);
+console.log(idade);
+console.log(matriculado);
+console.log(cidade);
+
+export {};
