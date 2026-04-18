2c6cd2024c9bd17f33a70ce3a9e1a1e848c1b2db
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-18 16:37:25 -0300

    Exercicio Variaveis em js

---
+++ Exercicio/Variaveis.js
@@ -0,0 +1,10 @@
+const nome = "Otavio";
+const idade = 18;
+const matriculado = true;
+const cidade = "Araras";
+
+
+console.log(nome);
+console.log(idade);
+console.log(matriculado);
+console.log(cidade);
