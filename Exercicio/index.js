3e52bd60bbc9f37c41a65820354f7547c68d917a
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-18 17:00:07 -0300

    chore: ajustar exercicio npm e ignorar node_modules

--- Exercicio/index.js
+++ Exercicio/index.js
@@ -1,9 +1,7 @@
 const chalk = require("chalk");
-
 const nome = "Otavio";
 const ra = "2901432613015";
 const disciplina = "Analise de Desenvolvimento de Sistemas";
-
 
 console.log(chalk.green(nome));
 console.log(chalk.yellow(ra));
