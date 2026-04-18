8834802672f1f2421f9c074353cd79eb343c1170
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-18 16:49:21 -0300

    index.js (NPM + Chalk)

---
+++ Exercicio/index.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+
+const nome = "Otavio";
+const ra = "2901432613015";
+const disciplina = "Analise de Desenvolvimento de Sistemas";
+
+
+console.log(chalk.green(nome));
+console.log(chalk.yellow(ra));
+console.log(chalk.blue(disciplina));
+console.log(chalk.red.bold("Exercício NPM concluído"));
